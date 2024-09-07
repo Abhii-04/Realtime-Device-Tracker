ae56396135e88ac9592d6322b53b8fe4d323108c
Abhishek Yadav <155748765+Abhii-04@users.noreply.github.com>
2024-09-07 18:09:39 +0530

    finally done

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const app = express();
-
-// Setting up server
 const http = require('http');
 const socketIo = require('socket.io');
 const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on('connection', function (socket) {
-    console.log('Connected');
+    console.log('Client connected:', socket.id);
+
     socket.on('sendLocation', function (data) {
-        console.log(`Received location: Latitude: ${data.latitude}, Longitude: ${data.longitude}`);
-        io.emit('receive-Location', {id:socket.id, data:data});
+        console.log(`Received location from ${socket.id}:`, data);
+        
+        
+        io.emit('receive-Location', { id: socket.id, ...data });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
     });
 });
 
-// importing HTML file
 app.use(express.static('public'));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Start the server
 server.listen(3000, function () {
-    console.log('Running on port :3000');
+    console.log('Server running on port 3000');
 });
--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ const socket = io();
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
-        console.log("Sending location:", { latitude, longitude }); // Log sent data
+        console.log("Sending location:", { latitude, longitude });
         socket.emit("sendLocation", { latitude, longitude });
     }, (error) => {
-        console.log(error);
+        console.log("Geolocation error:", error);
     }, {
         enableHighAccuracy: true,
         timeout: 5000,
@@ -14,29 +14,40 @@ if (navigator.geolocation) {
     });
 }
 
-// Map initialization
+
 const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: "OpenStreetMap"
 }).addTo(map);
 
-const markers = {};
+let markers = {};
 
-// Listen for location updates
+
 socket.on("receive-Location", (data) => {
     const { id, latitude, longitude } = data;
-    console.log("Received location:", { id, latitude, longitude }); // Log received data
+    console.log("Received location:", { id, latitude, longitude });
 
-    // Update map view to new location
-    map.setView([latitude, longitude], 10);
+    if (latitude === 0 && longitude === 0) {
+        console.log("Received invalid location data");
+        return;
+    }
 
-    // Update marker position or create a new marker if it doesn't exist
     if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
         console.log(`Updated marker for ${id}`);
     } else {
         markers[id] = L.marker([latitude, longitude]).addTo(map);
         console.log(`Created new marker for ${id}`);
+    }
+  
+    // update the map view to the new location
+    map.setView([latitude, longitude], 10);
+});
+
+socket.on("user-disconnected", (id) => {  
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        console.log(`Removed marker for ${id}`);
     }
 });
