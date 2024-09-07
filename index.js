1436069fdb410e15582f3da5b1827ceb34d253bc
Abhishek Yadav <155748765+Abhii-04@users.noreply.github.com>
2024-09-07 16:58:43 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// Setting up server
+const http = require('http');
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+    console.log('Connected');
+    socket.on('sendLocation', function (data) {
+        console.log(`Received location: Latitude: ${data.latitude}, Longitude: ${data.longitude}`);
+    });
+});
+
+// importing HTML file
+app.use(express.static('public'));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Start the server
+server.listen(3000, function () {
+    console.log('Running on port :3000');
+});
